a7495424a0b9e9a8c48c4322a1f3fee09babe289
tdronfield <t_dronfield@fanshaweonline.ca>
2020-11-09 10:48:11 -0500

    adding event delegation week 7adding event delegation week 7

--- includes/functions.php
+++ includes/functions.php
@@ -1,20 +1,45 @@
 <?php
-// include the file we just wrote - connect
-    include("connect.php");  // like a JS import statement
+// result will store the database request results 
+//so that we can encode and return them to index.php
 
-    $query = "SELECT * FROM profdata";
+   $result = array();
 
-    $runQuery = $pdo->query($query);
+   function getAllUsers($conn) {
+        // include("connect.php");  // like a JS import statement
 
-    $result = array();
+        $query = "SELECT * FROM profdata";
 
-    // go through database by row, then store that data into result array
-    // PDO::FETCH_ASSOC: returns an array indexed by column name as returned in your result set
-    while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
-        $result[] = $row;
+        $runQuery = $conn->query($query);
+
+        // go through database by row, then store that data into result array
+        // PDO::FETCH_ASSOC: returns an array indexed by column name as returned in your result set
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        //return $result;
+        echo(json_encode($result));
+        // on windows, use echo
+        // otherwise, may use return
+   }
+
+   
+   function getSingleUser($conn, $id) {
+        // include("connect.php");  // like a JS import statement
+
+        $query = "SELECT * FROM profdata WHERE id=" . $id . "";
+
+        $runQuery = $pdo->query($query);
+
+        // go through database by row, then store that data into result array
+        // PDO::FETCH_ASSOC: returns an array indexed by column name as returned in your result set
+        while($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
     }
 
     //return $result;
     echo(json_encode($result));
     // on windows, use echo
     // otherwise, may use return
+
+   }
--- js/main.js
+++ js/main.js
@@ -7,31 +7,49 @@ import {fetchData} from "./modules/DataMiner.js";
 (() => {
     // this receives the data payload from our AJAX req, parses it
     function handleDataSet(data) {
+        // populate a lightbox with this data
+        // then open it
+        let lightbox = document.querySelector(."lightbox");
+        
+    }
+
+    function retrieveProjectInfo() {
+        // console.log(event.target.id);
+        // debugger;
+
+        // check for an ID, if there isn't one, dont try fetch call (it would break)
+        if (!event.target.id) { return };
+
+        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
+
+    }
+
+    function renderPortfolioImages(thumb) {
         let userSection = document.querySelector(".users-section"),
             // Cannot reference template itself, need to select the content inside specifically
             userTemplate = document.querySelector("#profs-template").content;
 
-        debugger;
-
-        for (let user in data) {
-
-            debugger;
-
+        for (let user in thumb) {
 
             // clone the template, do a DEEP CLONE - taking the user div and copying it. A deep clone uses (true) to go inside the referenced node and find all the children inside it.
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children; 
-                // grabs all the children to utilize in the next statements
+                // grabs all the children to utilize in the next statement
 
-            currentUserText[1].src = `images/${data[user].avatar}`;
-            currentUserText[2].textContent = data[user].name;
-            currentUserText[3].textContent = data[user].role;
-            currentUserText[4].textContent = data[user].nickname;
-
+            currentUserText[1].src = `images/${thumb[user].avatar}`;
+            currentUserText[1].id = thumb[user].id;
+            
+            // add user to the view
             userSection.appendChild(currentUser);
         }
+
+        // adding a click event to the user Section wrapper
+        userSection.addEventListener("click", retrieveProjectInfo);
     }
 
     // pass in data that will be run through fetchData function in DataMiner.js
-    fetchData('./includes/functions.php').then(data => handleDataSet(data)).catch(err => console.log(err));
+    fetchData('./includes/index.php').then(data => renderPortfolioImages(data)).catch(err => console.log(err));
+
+    // fetchData('./includes/index.php').then(data => handleDataSet(data)).catch(err => console.log(err));
 })();
+
